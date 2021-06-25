a7730f3fc5eb63b02820b8e4efacb9ea75521375
remy bresson <remy.bresson@edifixio.com>
2021-06-25 10:51:46 +0200

    adding lambda code signature

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+myS3BucketForDeployment="s3://edfx-rbn-pileface/swagger/"
+
+signingName="PileFaceSigningProfile_LIRVA8ZRwYzt"
+signingVersionnedArn="arn:aws:signer:eu-west-3:361509912577:/signing-profiles/PileFaceSigningProfile_LIRVA8ZRwYzt/rSAd0xLAYa"
 
 currentDir=`pwd`
 
-aws s3 cp ../swaggers/pileface.yaml s3://edfx-rbn-pileface/swagger/
+aws s3 cp ../swaggers/pileface.yaml $myS3BucketForDeployment
 
 
 # Zip register lambda function
@@ -19,8 +23,5 @@ zip bet.zip *.py
 
 cd $currentDir
 
-
-sam package -t sam.yaml --s3-bucket edfx-rbn-pileface --output-template-file packagedsam.yaml 
-
-
-sam deploy -t packagedsam.yaml --capabilities  CAPABILITY_IAM CAPABILITY_NAMED_IAM --stack-name pileface --parameter-overrides UidDeployment=77
+sam package -t sam.yaml --signing-profiles BetBackend=$signingName RegisterBackend=$signingName --s3-bucket edfx-rbn-pileface --output-template-file packagedsam.yaml 
+sam deploy -t packagedsam.yaml  --capabilities  CAPABILITY_IAM CAPABILITY_NAMED_IAM --stack-name pileface --parameter-overrides UidDeployment=77 LambdaSigningProfileProfileVersionArn=$signingVersionnedArn
